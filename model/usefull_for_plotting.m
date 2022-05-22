bad40f83e1fcd76f2d57e1648267b591f9306c26
mastroalex <76497903+mastroalex@users.noreply.github.com>
2022-05-22 18:17:05 +0200

    new data

--- model/usefull_for_plotting.m
+++ model/usefull_for_plotting.m
@@ -30,7 +30,7 @@ ylabel('Flux [L/s]')
 grid on
 
 
-%% 
+%%  plot extra
 
 load("data_log/data_sine_wave_freq_30.mat")
 
@@ -57,5 +57,60 @@ ylim([-2.5;2.5])
 ylabel('Flux [L/s]')
 grid on
 
+%% plot extra
+
+load("data_log/data_sine_wave_freq_45.mat")
+
+% data clear 
+noise_point=find(mechanics_lung.Data(:,2)==min(mechanics_lung.Data(:,2)));
+% replace extremely noise point with the previous one
+
+mechanics_lung.Data(noise_point,2)=mechanics_lung.Data(noise_point-1,2); 
+figure()
+plot(mechanics_lung.Time,0.49*mechanics_lung.Data(:,1),'--')
+hold on
+for i=2:length(mechanics_lung.Data(1,:))
+    plot(mechanics_lung.Time,mechanics_lung.Data(:,i))
+end
+hold off
+legend({'P_{aO}*1/2','Flux', 'Volume'},'Location','southeast')
+xlabel('t[s]')
+title('Flow and volume')
+yyaxis left
+ylim([-2.5;2.5])
+ylabel('Volume [L]')
+yyaxis right
+ylim([-2.5;2.5])
+ylabel('Flux [L/s]')
+grid on
+
+%% plot square wave
+
+load("data_log/data_square_wave_freq_45.mat")
+
+
+
+% data clear 
+noise_point=find(mechanics_lung.Data(:,2)==min(mechanics_lung.Data(:,2)));
+% replace extremely noise point with the previous one
+
+mechanics_lung.Data(noise_point,2)=mechanics_lung.Data(noise_point-1,2); 
+figure()
+plot(mechanics_lung.Time,mechanics_lung.Data(:,1),'--')
+hold on
+for i=2:length(mechanics_lung.Data(1,:))
+    plot(mechanics_lung.Time,mechanics_lung.Data(:,i))
+end
+hold off
+legend({'P_{aO}','Flux', 'Volume'},'Location','southeast')
+xlabel('t[s]')
+title('Flow and volume')
+yyaxis left
+ylim([-6;6])
+ylabel('Volume [L]')
+yyaxis right
+ylim([-6;6])
+ylabel('Flux [L/s]')
+grid on
 %% 
 %exportgraphics(figure(7),'data_log/data_sine_wave_freq_15_zoom.pdf','BackgroundColor','none','ContentType','vector');
