9ab44d095e11266964f26341b1fa50f390c71d11
mastroalex <76497903+mastroalex@users.noreply.github.com>
2022-05-22 20:01:25 +0200

    other tests

--- model/pressure_vs_volume_plot.m
+++ model/pressure_vs_volume_plot.m
@@ -1,116 +1,69 @@
 % general plot file
 
 %% plot case 1
-
+close all
 load("data_log/data_sine_wave_freq_15.mat")
+mechanics_lung_1=mechanics_lung;
 
-
-
-% data clear 
-noise_point=find(mechanics_lung.Data(:,2)==min(mechanics_lung.Data(:,2)));
-% replace extremely noise point with the previous one
-
-mechanics_lung.Data(noise_point,2)=mechanics_lung.Data(noise_point-1,2); 
-figure()
-plot(mechanics_lung.Time,mechanics_lung.Data(:,1),'--')
-hold on
-for i=2:length(mechanics_lung.Data(1,:))
-    plot(mechanics_lung.Time,mechanics_lung.Data(:,i))
-end
-hold off
-legend({'P_{aO}','Flux', 'Volume'},'Location','southeast')
-xlabel('t[s]')
-title('Flow and volume')
-yyaxis left
-ylim([-1.5;1.5])
-ylabel('Volume [L]')
-yyaxis right
-ylim([-1.5;1.5])
-ylabel('Flux [L/s]')
-grid on
-
-
-%%  plot extra
-
-load("data_log/data_sine_wave_freq_30.mat")
+load("data_log/data_square_wave_freq_18.mat")
+mechanics_lung_2=mechanics_lung;
 
 % data clear 
-noise_point=find(mechanics_lung.Data(:,2)==min(mechanics_lung.Data(:,2)));
+noise_point=find(mechanics_lung_1.Data(:,2)==min(mechanics_lung_1.Data(:,2)));
+noise_point_1=find(mechanics_lung_2.Data(:,2)==min(mechanics_lung_2.Data(:,2)));
+
 % replace extremely noise point with the previous one
 
-mechanics_lung.Data(noise_point,2)=mechanics_lung.Data(noise_point-1,2); 
+mechanics_lung_1.Data(noise_point,2)=mechanics_lung_1.Data(noise_point-1,2); 
+mechanics_lung_2.Data(noise_point_square,2)=mechanics_lung_2.Data(noise_point_square-1,2); 
+
 figure()
-plot(mechanics_lung.Time,mechanics_lung.Data(:,1),'--')
+plot(mechanics_lung_1.Data(1:end/2,1),mechanics_lung_1.Data(1:end/2,3))
 hold on
-for i=2:length(mechanics_lung.Data(1,:))
-    plot(mechanics_lung.Time,mechanics_lung.Data(:,i))
-end
-hold off
-legend({'P_{aO}','Flux', 'Volume'},'Location','southeast')
-xlabel('t[s]')
-title('Flow and volume')
-yyaxis left
-ylim([-2.5;2.5])
+plot(mechanics_lung_2.Data(1:end/2,1),mechanics_lung_2.Data(1:end/2,3))
+xlabel('Pressure [cm H2O]')
 ylabel('Volume [L]')
-yyaxis right
-ylim([-2.5;2.5])
-ylabel('Flux [L/s]')
+title('Pressure vs volume')
 grid on
+box off
+legend({'P_{aO} \propto sine wave','P_{aO} \propto square wave'},'Location','southeast')
+exportgraphics(figure(1),'data_log/PV_sine_vs_square.pdf','BackgroundColor','none','ContentType','vector');
 
-%% plot extra
+%%
+close all
+load("data_log/data_square_wave_rise_30_Tinsp_40_PEEP_5_Paw_25_freq_18.mat")
+mechanics_lung_1=mechanics_lung;
 
-load("data_log/data_sine_wave_freq_45.mat")
+load("data_log/data_square_wave_rise_0_Tinsp_40_PEEP_5_Paw_25_freq_18.mat")
+mechanics_lung_2=mechanics_lung;
+
+load("data_log/data_square_wave_rise_15_Tinsp_40_PEEP_5_Paw_25_freq_18.mat")
+mechanics_lung_3=mechanics_lung;
 
 % data clear 
-noise_point=find(mechanics_lung.Data(:,2)==min(mechanics_lung.Data(:,2)));
+noise_point=find(mechanics_lung_1.Data(:,2)==min(mechanics_lung_1.Data(:,2)));
+noise_point_1=find(mechanics_lung_2.Data(:,2)==min(mechanics_lung_2.Data(:,2)));
+noise_point_2=find(mechanics_lung_3.Data(:,2)==min(mechanics_lung_3.Data(:,2)));
+
 % replace extremely noise point with the previous one
 
-mechanics_lung.Data(noise_point,2)=mechanics_lung.Data(noise_point-1,2); 
+mechanics_lung_1.Data(noise_point,2)=mechanics_lung_1.Data(noise_point-1,2); 
+mechanics_lung_2.Data(noise_point_1,2)=mechanics_lung_2.Data(noise_point_1-1,2); 
+mechanics_lung_3.Data(noise_point_2,2)=mechanics_lung_3.Data(noise_point_2-1,2); 
+
 figure()
-plot(mechanics_lung.Time,0.49*mechanics_lung.Data(:,1),'--')
+plot(mechanics_lung_1.Data(1:end/2,1),mechanics_lung_1.Data(1:end/2,3))
 hold on
-for i=2:length(mechanics_lung.Data(1,:))
-    plot(mechanics_lung.Time,mechanics_lung.Data(:,i))
-end
-hold off
-legend({'P_{aO}*1/2','Flux', 'Volume'},'Location','southeast')
-xlabel('t[s]')
-title('Flow and volume')
-yyaxis left
-ylim([-2.5;2.5])
+plot(mechanics_lung_3.Data(1:end/2,1),mechanics_lung_3.Data(1:end/2,3))
+
+plot(mechanics_lung_2.Data(1:end/2,1),mechanics_lung_2.Data(1:end/2,3))
+
+xlabel('Pressure [cm H2O]')
 ylabel('Volume [L]')
-yyaxis right
-ylim([-2.5;2.5])
-ylabel('Flux [L/s]')
+title('Pressure vs volume')
 grid on
+box off
+legend({'\tau = 0.3 * T_{insp}','\tau = 0.1 * T_{insp}','\tau = 0'},'Location','north')
 
-%% plot square wave
-
-load("data_log/data_square_wave_freq_45.mat")
-
-
-
-% data clear 
-noise_point=find(mechanics_lung.Data(:,2)==min(mechanics_lung.Data(:,2)));
-% replace extremely noise point with the previous one
-
-mechanics_lung.Data(noise_point,2)=mechanics_lung.Data(noise_point-1,2); 
-figure()
-plot(mechanics_lung.Time,mechanics_lung.Data(:,1),'--')
-hold on
-for i=2:length(mechanics_lung.Data(1,:))
-    plot(mechanics_lung.Time,mechanics_lung.Data(:,i))
-end
-hold off
-legend({'P_{aO}','Flux', 'Volume'},'Location','southeast')
-xlabel('t[s]')
-title('Flow and volume')
-yyaxis left
-ylim([-6;6])
-ylabel('Volume [L]')
-yyaxis right
-ylim([-6;6])
-ylabel('Flux [L/s]')
-grid on
 %% 
 %exportgraphics(figure(7),'data_log/data_sine_wave_freq_15_zoom.pdf','BackgroundColor','none','ContentType','vector');
